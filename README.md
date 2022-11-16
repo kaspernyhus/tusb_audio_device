07e802161451a2c78ebd3bbc22bea5cc20cd80b8
Kasper Nyhus <kanyhus@gmail.com>
2022-11-16 21:21:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+| Supported Targets | ESP32-S2 | ESP32-S3 |
+| ----------------- | -------- | -------- |
+
+# TinyUSB Audio Device
+
---
+++ main/main.c
@@ -0,0 +1,20 @@
+/* USB Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdint.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "USB audio example";
+
+void app_main(void)
+{
+}
