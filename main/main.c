ddee20f6c2f3dd7dbc644de11b877657b2f93380
Kasper Nyhus <kanyhus@gmail.com>
2023-03-14 21:25:16 +0100

    add signal gen &  USB Audio

--- main/main.c
+++ main/main.c
@@ -16,15 +16,30 @@
 #include "sdkconfig.h"
 
 #include "tinyusb.h"
+#include "tusb_audio.h"
 #include "tusb_net.h"
 
+#include "esp_signal_generator.h"
+
 static const char *TAG = "USB audio example";
+
+esp_sig_gen_t sig_gen;
+uint8_t audio_data[CFG_TUD_AUDIO_EP_SZ_IN];
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "------------ app_main -----------");
 
+#if CONFIG_ESP_TINYUSB_AUDIO_ENABLED
+    esp_sig_gen_config_t sig_gen_cfg = {}; // default configuration
+    esp_sig_gen_init(&sig_gen, &sig_gen_cfg);
+    tusb_audio_init();
+#endif
+
+
+#if CONFIG_ESP_TINYUSB_NET_ENABLED
     tusb_net_init();
+#endif
 
     ESP_LOGI(TAG, "USB initialization");
     // Install TINYUSB driver
@@ -34,4 +49,36 @@ void app_main(void)
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
 
     ESP_LOGI(TAG, "USB initialization DONE");
+}
+
+// ----------- USB Callbacks ----------- //
+void tud_mount_cb(void)
+{
+    ESP_LOGI(TAG, "USB mounted.");
+}
+
+// ----------- USB Audio Callbacks ----------- //
+bool tud_audio_tx_done_pre_load_cb(uint8_t rhport, uint8_t itf, uint8_t ep_in, uint8_t cur_alt_setting)
+{
+    (void)rhport;
+    (void)itf;
+    (void)ep_in;
+    (void)cur_alt_setting;
+
+    tud_audio_write(audio_data, CFG_TUD_AUDIO_EP_SZ_IN);
+
+    return true;
+}
+
+bool tud_audio_tx_done_post_load_cb(uint8_t rhport, uint16_t n_bytes_copied, uint8_t itf, uint8_t ep_in, uint8_t cur_alt_setting)
+{
+    (void)rhport;
+    (void)n_bytes_copied;
+    (void)itf;
+    (void)ep_in;
+    (void)cur_alt_setting;
+
+    esp_sig_gen_fill(&sig_gen, audio_data,CFG_TUD_AUDIO_EP_SZ_IN, 48*2);
+
+    return true;
 }
